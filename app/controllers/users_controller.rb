4a0b2e52c7591110d6a80a88f0a8561f13cd452f
lpryst <lilipryst23@gmail.com>
2017-07-09 13:27:44 +0300

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Talky App!"
       redirect_to @user
     else
